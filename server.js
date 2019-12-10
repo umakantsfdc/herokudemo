9db0e339017e3f0d7e969ca2b991646a7472f51e
umakantsfdc <umakantsuman@gmail.com>
2019-12-10 22:46:45 +0530

    Add files via upload

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ app.set('port', process.env.PORT || 5000);
 app.use(express.static('public'));
 app.use(bodyParser.json());
 
-app.post('/update', function(req, res) {
+app.post('/createLead', function(req, res) {
     pg.connect(process.env.DATABASE_URL, function (err, conn, done) {
         // watch for any connect issues
         if (err) console.log(err);
